5bfede88cff110f962c451108b5049545ca70551
Sergeydf <ser.marmelad@mail.ru>
2019-11-11 10:36:57 +0300

    parser final

--- bot/psrser_test.py
+++ bot/psrser_test.py
@@ -1,22 +1,42 @@
+#импорт библиотек
 import requests
 from bs4 import BeautifulSoup as bs
+
 
 headers = {'accept': '*/*',
                 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
 
-base_url = 'https://info.olimpiada.ru/activities'
-def hh_parse(base_url, headers):
+
+#указываем ссылку на сайт
+base_url = 'https://olimpiada.ru/activities'
+#основаня функция
+def ol_parse(base_url, headers):
     session = requests.Session()
     request = session.get(base_url,headers=headers)
+    #делаем запрос
     if request.status_code == 200:
         soup = bs(request.content, 'html.parser')
-        divs = soup.find_all('td')
+        divs_2 = soup.find_all('div', attrs={'class': 'tl_event long'})
+        divs = soup.find_all('div', attrs={'class': 'fav_olimp olimpiada'})
+        #достаем дату
+        for div in divs_2:
+            date = div.find('span', attrs={'class': 'tl_cont_s'}).text
+            print(date)
+        #достаем ссылку название рейтинг и класс
         for div in divs:
-            title = div.find('a', attrs={'class': 'black_hover_blue'}).text
-            href = div.find('a', attrs={'class': 'black_hover_blue'})["href"]
+
+            title = div.find('span', attrs={'class': 'headline'}).text
+            href = div.find('a', attrs={'class': 'none_a black'})["href"]
+            classes = div.find('span', attrs={'class': 'classes_dop'}).text
+            rait = div.find('span', attrs={'class': 'pl_rating'}).text
+
             print(title)
-            print(href)
+            print("https://olimpiada.ru"+href)
+            print(classes)
+            print(rait)
+    #если не подключились
     else:
         print("NEOK")
 
-hh_parse(base_url, headers)
+#вызываем функцию
+ol_parse(base_url, headers)
