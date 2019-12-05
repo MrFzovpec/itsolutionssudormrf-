dfe405d647f80fc7079400af87cfbe3b95053759
Your Name <you@example.com>
2019-12-05 18:10:24 +0300

    Главная страница

--- bot/an_parser.py
+++ bot/an_parser.py
@@ -1,30 +1,37 @@
-#импорт библиотек
+# импорт библиотек
 import requests
 from bs4 import BeautifulSoup as bs
 
 
 headers = {'accept': '*/*',
-                'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
+           'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
 
 
-#указываем ссылку на сайт
+# указываем ссылку на сайт
 base_url = 'https://hh.ru/search/vacancy?area=1&st=searchVacancy&text=Java'
-#основаня функция
+# основаня функция
+
+
 def ol_parse(base_url, headers):
     session = requests.Session()
-    request = session.get(base_url,headers=headers)
+    request = session.get(base_url, headers=headers)
     jobs = []
-    #делаем запрос
+    # делаем запрос
     if request.status_code == 200:
         soup = bs(request.content, 'html.parser')
         divs = soup.find_all('div', attrs={'class': 'vacancy-serp-item'})
 
         for div in divs:
-            title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
-            href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})["href"]
-            company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-            disc_1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
-            disc_2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+            title = div.find(
+                'a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+            href = div.find(
+                'a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})["href"]
+            company = div.find(
+                'a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+            disc_1 = div.find(
+                'div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+            disc_2 = div.find(
+                'div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
             disc = disc_1 + ' ' + disc_2
             jobs.append({
                 'title': title,
@@ -32,30 +39,30 @@ def ol_parse(base_url, headers):
                 'company': company,
                 'disc': disc
             })
-        for i in range(0,len(jobs)):
+        for i in range(0, len(jobs)):
             print(jobs[i])
-            i+=1
+            i += 1
         print(len(jobs))
 
-        #достаем дату
+        # достаем дату
 
-        #достаем ссылку название рейтинг и класс
-        #for div in divs:
+        # достаем ссылку название рейтинг и класс
+        # for div in divs:
 
-            #title = div.find('h2', attrs={'class': 'event__name'}).text
-            #org = div.find('div', attrs={'class': 'event__description-value'}).text
-            #href = div.find('a', attrs={'class': 'event__name-link'})["href"]
-            #classes = div.find('div', attrs={'class': 'event__description-value'}).text
-            #rait = div.find('span', attrs={'class': 'pl_rating'}).text
+        #title = div.find('h2', attrs={'class': 'event__name'}).text
+        #org = div.find('div', attrs={'class': 'event__description-value'}).text
+        #href = div.find('a', attrs={'class': 'event__name-link'})["href"]
+        #classes = div.find('div', attrs={'class': 'event__description-value'}).text
+        #rait = div.find('span', attrs={'class': 'pl_rating'}).text
 
-
-            #print(org)
-            #print("https://olimpiada.ru"+href)
-            #print(classes)
-            #print(rait)
-    #если не подключились
+        # print(org)
+        # print("https://olimpiada.ru"+href)
+        # print(classes)
+        # print(rait)
+    # если не подключились
     else:
         print("NEOK")
 
-#вызываем функцию
+
+# вызываем функцию
 ol_parse(base_url, headers)
--- bot/psrser_test.py
+++ bot/psrser_test.py
@@ -1,31 +1,33 @@
-#импорт библиотек
+# импорт библиотек
 import requests
 from bs4 import BeautifulSoup as bs
 
 
 headers = {'accept': '*/*',
-                'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
+           'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
 
 
-#указываем ссылку на сайт
+# указываем ссылку на сайт
 base_url = 'https://olimpiada.ru/activities'
-#основаня функция
+# основаня функция
+
+
+
 def ol_parse(base_url, headers):
     session = requests.Session()
-    request = session.get(base_url,headers=headers)
+    request = session.get(base_url, headers=headers)
     full = []
-    #делаем запрос
+    # делаем запрос
     if request.status_code == 200:
         soup = bs(request.content, 'html.parser')
         divs_2 = soup.find_all('div', attrs={'class': 'tl_event long'})
         divs = soup.find_all('div', attrs={'class': 'fav_olimp olimpiada'})
-        #достаем дату
+        # достаем дату
         for div in divs_2:
             date = div.find('span', attrs={'class': 'tl_cont_s'}).text
             print(date)
 
-
-        #достаем ссылку название рейтинг и класс
+        # достаем ссылку название рейтинг и класс
         for div in divs:
 
             title = div.find('span', attrs={'class': 'headline'}).text
@@ -34,16 +36,17 @@ def ol_parse(base_url, headers):
             rait = div.find('span', attrs={'class': 'pl_rating'}).text
 
             full.append({
-            'title': title,
-            'href': "https://olimpiada.ru"+href,
-            'classes': classes,
-            'rait': rait
+                'title': title,
+                'href': "https://olimpiada.ru" + href,
+                'classes': classes,
+                'rait': rait
             })
-        for i in range(0,len(full)):
+        for i in range(0, len(full)):
             print(full[i])
-    #если не подключились
+    # если не подключились
     else:
         print("NEOK")
 
-#вызываем функцию
+
+# вызываем функцию
 ol_parse(base_url, headers)
